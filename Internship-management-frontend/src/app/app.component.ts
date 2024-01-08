cb34a5703a3b29aaab448da3c87ae8f39c40161c
oumaa17 <nadiroumaima17@gmail.com>
2024-01-08 23:50:44 +0100

    Adding untracked files

--- Internship-management-frontend/src/app/app.component.ts
+++ Internship-management-frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AcceuilComponent } from './components/acceuil/acceuil.component';
+import { FilesSubmissionComponent } from './components/files-submission/files-submission.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  
+  imports: [CommonModule, RouterOutlet,NavBarComponent,AcceuilComponent,FilesSubmissionComponent],
+  
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+ 
 })
 export class AppComponent {
   title = 'Internship-management-frontend';
--- Internship-management-frontend/src/app/app.routes.ts
+++ Internship-management-frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AcceuilComponent } from './components/acceuil/acceuil.component';
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
+import {LoginComponent} from "./login/login.component";
+import { FilesSubmissionComponent } from './components/files-submission/files-submission.component';
 
-export const routes: Routes = [];
+
+
+
+
+export const routes: Routes = [
+  { path: '', component: AcceuilComponent }, // Home page route
+  {
+    path: "navbar", component: NavBarComponent, //user
+   },
+  {path: "acceuil", component: AcceuilComponent},
+  {path: "FilesSubmission", component: FilesSubmissionComponent},
+
+      
+  
+
+
+{ path: 'login', component: LoginComponent },
+];
